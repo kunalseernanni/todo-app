cefbc5f80f344c345491a99c94c9f79b1c7cd8a9
APPLE <apple@APPLEs-MacBook-Pro.local>
2024-06-16 08:19:41 +0530

    Added post, put , put

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { createTodo, updateTodo } = require("./types");
+
 const express= rwequire('express');
 const app = express();
 
@@ -7,6 +9,17 @@ app.use(express.json());
 // body {
 //     
 app.post("/todo", function(req, res){
+    const payload= req.body
+    const parsedpayload= createTodo.safeParse(payload) // parse
+
+    if (!parsedpayload.success){
+        res,status(411).json({
+            mssg: "invalid payload"
+        })
+
+        return;
+    }
+
 
 })
 
@@ -16,4 +29,12 @@ app.get(".todos", function(req, res){
 
 app.put("/done", function(req, res){
 
+    const updatepayload= updateTodo.safeParse(req.body)
+
+    if (!updatepayload.success){
+        res,status(411).json({
+            mssg: "invalid payload"
+        })
+    }
+    return
 })
