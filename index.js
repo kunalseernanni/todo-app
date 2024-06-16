fc10161f5eafdf7d695930c202be7d6a4d628aa0
APPLE <apple@APPLEs-MacBook-Pro.local>
2024-06-16 07:53:08 +0530

    New-year-app

---
+++ index.js
@@ -0,0 +1,19 @@
+const express= rwequire('express');
+const app = express();
+
+app.use(express.json());
+
+
+// body {
+//     
+app.post("/todo", function(req, res){
+
+})
+
+app.get(".todos", function(req, res){
+
+})
+
+app.put("/done", function(req, res){
+
+})
